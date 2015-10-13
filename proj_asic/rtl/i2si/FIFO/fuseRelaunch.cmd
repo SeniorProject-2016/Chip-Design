006a924288eaf3979f633ad355127d120980e67e
Zachary Nelson <nelsonz1@tcnj.edu>
2015-10-13 13:46:12 -0400

    Added FIFO Project Files

---
+++ proj_asic/rtl/i2si/FIFO/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Zachary/Zachs_FIFO/fifo_test_isim_beh.exe" -prj "C:/Users/Zachary/Zachs_FIFO/fifo_test_beh.prj" "work.fifo_test" "work.glbl" 
