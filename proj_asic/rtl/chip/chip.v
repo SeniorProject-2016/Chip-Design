47a2b1f12e9badb873056b9942d3a4eb2f74ebe7
Zachary Nelson <nelsonz1@tcnj.edu>
2016-01-13 10:26:18 -0500

    Added Filter Interfaces to chip.v

--- proj_asic/rtl/chip/chip.v
+++ proj_asic/rtl/chip/chip.v
@@ -2,19 +2,20 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Module Name:             chip.v
 // Create Date:             12/20/2015
-// Last Modification:       1/12/2015
+// Last Modification:       1/13/2015
 // Author:                  Zachary Nelson
 //
 //////////////////////////////////////////////////////////////////////////////////
 
 module chip(clk,rst_n,inp_sck,inp_ws,inp_sd,rf_i2si_en,rf_bist_start_val,
-            rf_bist_inc,rf_bist_up_limit,rf_mux_en,i2si_rtr,i2so_sck,i2so_ws,
+            rf_bist_inc,rf_bist_up_limit,rf_mux_en,i2so_sck,i2so_ws,
             i2so_sd,trig_fifo_overrun_clr,ro_fifo_overrun,trig_fifo_underrun,
-            ro_fifo_underrun,filt_rtr);
+            ro_fifo_underrun,rf_filter_shift,rf_filter_clip_en,rf_filter_coeffs);
             
-    // INPUTS AND OUTPUTS
+    // CHIP INTERFACES
     //---------------------------------------------------------------------------  
     //---------------------------------------------------------------------------  
+    // General
     input clk;                          // master clock
     input rst_n;                        // reset
     
@@ -43,23 +44,31 @@ module chip(clk,rst_n,inp_sck,inp_ws,inp_sd,rf_i2si_en,rf_bist_start_val,
     input [7:0] rf_bist_inc;            // BIST increment value
     input [31:0] rf_bist_up_limit;      // BIST upper limit value
     input rf_mux_en;                    // multiplexer select bit
-    input i2si_rtr;                     // I2S ready to receive
+    wire i2si_rtr;                      // I2S ready to receive
         
     // Inputs to I2S Output Block
     wire sck_inp;                       // synchronized serial clock
     wire sck_transition;                // synchronized serial clock transition
+    wire [31:0] filt_out_data;          // I2SO input data
     
     // Inputs to Filter Block
     wire filt_rts;                      // I2S input ready to send
-    wire [31:0] filt_data;              // I2S input output data
-    output filt_rtr;
+    wire [31:0] filt_input_data;        // I2SI output data
+    wire filt_rtr;
+    input rf_filter_shift;              // # of bits to shift after accumulator
+    input rf_filter_clip_en;            // select bit (1- perform clipping, 0- no clipping)
+    input [7:0] rf_filter_coeffs;       // filter coeffs
     
     // Inputs to Register Block
     output ro_fifo_overrun;             // when the I2S input FIFO is full
     output ro_fifo_underrun;            // FIFO buffer is not full and no more data is available
     //---------------------------------------------------------------------------  
     //---------------------------------------------------------------------------  
-      
+    
+    
+    // MODULE INSTANTIATION
+    //---------------------------------------------------------------------------  
+    //---------------------------------------------------------------------------     
     i2s_in I2S_Input(
         .clk                     (clk),                     // input: master clock
         .rst_n                   (rst_n),                   // input: reset
@@ -72,7 +81,7 @@ module chip(clk,rst_n,inp_sck,inp_ws,inp_sd,rf_i2si_en,rf_bist_start_val,
         .rf_bist_up_limit        (rf_bist_up_limit),        // input: BIST upper limit value
         .rf_mux_en               (rf_mux_en),               // input: multiplexer select bit 
         .i2si_rtr                (i2si_rtr),                // input: ready to receive
-        .i2si_data               (filt_data),               // output: audio data
+        .i2si_data               (filt_input_data),         // output: audio data
         .i2si_rts                (filt_rts),                // output: ready to send
         .trig_fifo_overrun_clr   (trig_fifo_overrun_clr),   // input: signal to reset ro_fifo_overrun
         .ro_fifo_overrun         (ro_fifo_overrun),         // output: when the I2S input FIFO is full
@@ -85,9 +94,9 @@ module chip(clk,rst_n,inp_sck,inp_ws,inp_sd,rf_i2si_en,rf_bist_start_val,
         .rst_n                   (rst_n),                   // input: reset
         .sck_inp                 (sck_inp),                 // input: synchronized serial clock
         .sck_transition          (sck_transition),          // input: serial clock transition
-        .filt_rts                (filt_rts),                // input: ready to send
-        .filt_data               (filt_data),               // input: audio data
-        .filt_rtr                (filt_rtr),                // output: read to receive
+        .filt_rts                (aud_out_rts),             // input: ready to send
+        .filt_data               (filt_out_data),           // input: audio data
+        .filt_rtr                (filt_rtr),                // output: ready to receive
         .i2so_ws                 (i2so_ws),                 // output: word select
         .i2so_sd                 (i2so_sd),                 // output: serial data
         .i2so_sck                (i2so_sck),                // output: serial clock
@@ -96,7 +105,25 @@ module chip(clk,rst_n,inp_sck,inp_ws,inp_sd,rf_i2si_en,rf_bist_start_val,
     );
     
     filter Filter(
-    
+        .clk                     (clk),                     // input: master clock
+        .rstb                    (rst_n),                   // input: reset
+        .aud_in                  (filt_input_data),         // input: input parallel digital audio
+        .aud_in_rts              (filt_rts),                // input: ready to send (for input FIFO)
+        .aud_in_rtr              (i2si_rtr),                // output: ready to receive (for input FIFO)
+        .aud_out                 (filt_out_data),           // output: output parallel digital audio
+        .aud_out_rts             (aud_out_rts),             // output: ready to send (for output FIFO)
+        .aud_out_rtr             (filt_rtr),                // input: ready to receive (for output FIFO)
+        .rf_filter_shift         (rf_filter_shift),         // input: # of bits to shift after accumulator
+        .rf_filter_clip_en       (rf_filter_clip_en),       // input: select bit (1- perform clipping, 0- no clipping)
+        .rf_filter_coeffs        (rf_filter_coeffs)         // input: filter coeffs
     );
+    
+    /*register Register(
+        .clk                     (clk),                     // input: master clock
+        .rst                     (rst_n),                   // input: reset
+        
+    );*/
+    //---------------------------------------------------------------------------  
+    //---------------------------------------------------------------------------
        
 endmodule
---
+++ proj_asic/rtl/chip/filter.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name:             filter.v
+// Create Date:             
+// Last Modification:       1/12/2015
+// Author:                  Dhruvit Naik
+// Description: 
+//////////////////////////////////////////////////////////////////////////////////
+
+module filter(clk, rstb, aud_in, aud_in_rts, aud_in_rtr, aud_out, aud_out_rts,aud_out_rtr, rf_filter_shift, rf_filter_clip_en, rf_filter_coeffs);
+             
+    input 	clk;                                         // master clock
+    input 	rstb;                                        // reset
+    input 	[31:0] aud_in;                               // input parallel digital audio
+    input 	aud_in_rts;                                  // ready to send (input)
+    output 	aud_in_rtr;                                  // ready to recieve (input)
+    output 	[31:0] aud_out;                              // output parallel digital audio
+    output 	aud_out_rts;                                 // ready to send (output)
+    input 	aud_out_rtr;                                 // ready to recieve (output)
+    input	rf_filter_shift;                             // # of bits to shift after accumulator
+    input	rf_filter_clip_en;                           // select bit (1- perform clipping, 0- no clipping)
+    input [7:0] rf_filter_coeffs;                         // filter coeffs 
+    
+endmodule
