9156fcd13cf2b6d2e84a7e8d702f3c41dd99b83e
Ian Patel <ian.patel@yahoo.com>
2016-03-02 19:52:13 -0500

    receiving still not working

--- proj_asic/psoc/Wave Generator/Wave Generator.cydsn/main.c
+++ proj_asic/psoc/Wave Generator/Wave Generator.cydsn/main.c
@@ -13,47 +13,53 @@
 #include <project.h>
 #include <stdio.h>
 
-static void DmaRxConfiguration(void);
-static void DmaTxConfiguration(void);
+void DmaRxConfiguration(void);
+void DmaTxConfiguration(void);
 
 /* DMA Configuration for RxDMA */
-#define RxDMA_BYTES_PER_BURST       (2u)
-#define RxDMA_REQUEST_PER_BURST     (1u)
+#define RxDMA_BYTES_PER_BURST       2
+#define RxDMA_REQUEST_PER_BURST     1
 #define RxDMA_SRC_BASE              (CYDEV_PERIPH_BASE)
 #define RxDMA_DST_BASE              (CYDEV_SRAM_BASE)
 
 /* DMA Configuration for TxDMA */
-#define TxDMA_BYTES_PER_BURST       (2u)
-#define TxDMA_REQUEST_PER_BURST     (1u)
+#define TxDMA_BYTES_PER_BURST       2
+#define TxDMA_REQUEST_PER_BURST     1
 #define TxDMA_SRC_BASE              (CYDEV_PERIPH_BASE)
 #define TxDMA_DST_BASE              (CYDEV_PERIPH_BASE)
 
 /* Variable declarations for RxDMA */
-static uint8_t RxDMA_Chan;
-static uint8_t RxDMA_TD[1];
+uint8_t RxDMA_Chan;
+uint8_t RxDMA_TD[1];
 
 /* Variable declarations for TxDMA */
-static uint8_t TxDMA_0_Chan;
-static uint8_t TxDMA_0_TD[1];    
-static uint8_t TxDMA_1_Chan;
-static uint8_t TxDMA_1_TD[1];
+uint8_t TxDMA_0_Chan;
+uint8_t TxDMA_0_TD[1];    
+uint8_t TxDMA_1_Chan;
+uint8_t TxDMA_1_TD[1];
 
-static uint16_t ReceivedData[16384];    /* IN buffer */
+uint16_t ReceivedData[16384] = {0};    /* IN buffer */
+
+#if (CY_PSOC5LP)
+	#if !defined(DMA_DISABLE_TD)
+	    #define DMA_DISABLE_TD          0xFEu
+	#endif
+#endif  /* (CY_PSOC5LP) */
+
 
 int main()
 {
 	uint32_t i;
-	for(i = 0; i < 16384; i++)
-    {
-        ReceivedData[i] = 0;
-    }
+
+	/* Enable global interrupts */
+	//CyGlobalIntEnable;
 	
-	CyGlobalIntEnable;
-	
-	WaveDAC8_Start(); // Start the waveform DAC
+	/* Start the waveform DAC and ADC */
+	WaveDAC8_Start(); 
 	ADC_Start();
-	
 	ADC_StartConvert();
+	LCD_Start();
+	LCD_ClearDisplay();
 	
 	 /* Enable I2S component */
     I2S_Start();
@@ -62,18 +68,20 @@ int main()
     DmaRxConfiguration();
     DmaTxConfiguration();
 	
-	I2S_EnableRx();
+	I2S_EnableRx();		/* Enable Rx direction */
     I2S_EnableTx();     /* Enable Tx direction */
    
 	CyDelay(5000);		/* Wait for data transmissions to complete */
 	
+	//I2S_Stop();			/* Stop the I2S component */
+	
 	UART_Start();     	/* Enabling the UART */
 	
-	// Send the data through the UART component
+	/* Send the data to computer through the UART component, serial to USB */
 	for(i = 0; i < 16384; i++)
 	{
-		uint8_t part1 = (ReceivedData[i] >> 8) & 0x00ff;		// get first byte
-		uint8_t part2 = (ReceivedData[i] >> 0) & 0x00ff;		// get second byte
+		uint8_t part1 = (ReceivedData[i] >> 8) & 0x00ff;		/* get first byte */
+		uint8_t part2 = (ReceivedData[i] >> 0) & 0x00ff;		/* get second byte */
 		char part1_string[5];
 		char part2_string[5];
 		sprintf(part1_string, "%d", part1);
@@ -81,25 +89,27 @@ int main()
 		UART_PutString(part1_string);					
 		UART_PutString(", ");							
 		UART_PutString(part2_string);					
-		UART_PutString("\n\r");							
+		UART_PutString("\n\r");		
 	}
+	
 	for(;;);
 }
 
-// Rx DMA Config
+/* Rx DMA Config */
 void DmaRxConfiguration(void)
 {
-    // Init DMA, 2 byte bursts, each burst requires a request
+    /* Init DMA, 2 byte bursts, each burst requires a request */
     RxDMA_Chan = RxDMA_DmaInitialize(RxDMA_BYTES_PER_BURST, RxDMA_REQUEST_PER_BURST, 
                                      HI16(RxDMA_SRC_BASE), HI16(RxDMA_DST_BASE));
 	
-    RxDMA_TD[0u] = CyDmaTdAllocate();
+    RxDMA_TD[0] = CyDmaTdAllocate();
 	
-	// Configure this Td chain
-    CyDmaTdSetConfiguration(RxDMA_TD[0], 2, RxDMA_TD[0], TD_INC_DST_ADR);
-	
+	/* Configure this Td chain, get 16384 samples */
+    //CyDmaTdSetConfiguration(RxDMA_TD[0], 32768, RxDMA_TD[0], TD_INC_DST_ADR);
+	CyDmaTdSetConfiguration(RxDMA_TD[0], 32768, RxDMA_TD[0], TD_INC_DST_ADR);
+
     /* From the I2S to the memory */
-    CyDmaTdSetAddress(RxDMA_TD[0], LO16((uint32)I2S_RX_CH0_F0_PTR), LO16((uint32)ReceivedData));
+    CyDmaTdSetAddress(RxDMA_TD[0], LO16((uint32)ADC_DEC_SAMP_PTR), LO16((uint32)ReceivedData));
 	
     /* Associate the TD with the channel */
     CyDmaChSetInitialTd(RxDMA_Chan, RxDMA_TD[0]);
@@ -108,10 +118,10 @@ void DmaRxConfiguration(void)
     CyDmaChEnable(RxDMA_Chan, 1);
 }
 
-// Tx DMA Config
+/* Tx DMA Config */
 void DmaTxConfiguration(void)
 {
-    // Init DMA, 2 byte bursts, each burst requires a request 
+    /* Init DMA, 2 byte bursts, each burst requires a request */
     TxDMA_0_Chan = TxDMA_0_DmaInitialize(TxDMA_BYTES_PER_BURST, TxDMA_REQUEST_PER_BURST, 
                                     HI16(TxDMA_SRC_BASE), HI16(TxDMA_DST_BASE));
 	TxDMA_1_Chan = TxDMA_1_DmaInitialize(TxDMA_BYTES_PER_BURST, TxDMA_REQUEST_PER_BURST, 
@@ -120,21 +130,21 @@ void DmaTxConfiguration(void)
     TxDMA_0_TD[0] = CyDmaTdAllocate();
     TxDMA_1_TD[0] = CyDmaTdAllocate();
 
-	// Configure this Td chain
+	/* Configure this Td chain */
     CyDmaTdSetConfiguration(TxDMA_0_TD[0], 2, TxDMA_0_TD[0], TxDMA_0__TD_TERMOUT_EN);
     CyDmaTdSetConfiguration(TxDMA_1_TD[0], 2, TxDMA_1_TD[0], TxDMA_1__TD_TERMOUT_EN);
 	
-	// From the ADC to the I2S
+	/* From the ADC to the I2S */
     CyDmaTdSetAddress(TxDMA_0_TD[0], LO16((uint32)ADC_DEC_SAMP_PTR), LO16((uint32)I2S_TX_CH0_F0_PTR));
     CyDmaTdSetAddress(TxDMA_1_TD[0], LO16((uint32)ADC_DEC_SAMP_PTR), LO16((uint32)I2S_TX_CH0_F1_PTR));
 
-	// Associate the TD with the channel
+	/* Associate the TD with the channel */
     CyDmaChSetInitialTd(TxDMA_0_Chan, TxDMA_0_TD[0]);
     CyDmaChSetInitialTd(TxDMA_1_Chan, TxDMA_1_TD[0]);
 
-	// Enable the channel
+	/* Enable the channel */
     CyDmaChEnable(TxDMA_0_Chan, 1);
     CyDmaChEnable(TxDMA_1_Chan, 1);
 }
 
-/* [] END OF FILE */
+/* END OF FILE */
