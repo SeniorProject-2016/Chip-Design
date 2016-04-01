d50db63df4bad62b2d8a5318110abdb200553e48
dhruvit <naikd2@apps.tcnj.edu>
2016-04-01 19:25:52 -0400

    Updated filter_round_truncate block

--- proj_asic/rtl/Source Code/RTL/filter_round_truncate.v
+++ proj_asic/rtl/Source Code/RTL/filter_round_truncate.v
@@ -1,12 +1,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Module Name:             filter_round_truncate.v
 // Create Date:             ??? 
-// Last Modification:       3/25/2016
+// Last Modification:       4/1/2016
 // Author:                  Dhruvit Naik
 // Description:             ????
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
+
+// ext_acc_in, acc_r should be 43 bits, not 47 bits
+// Reasoning:
+//      12-19 bits lopped off during shift
+//      16 bits to keep
+//      8 bits of integer overflow, used for clipping
+//  MAX needed is 8 + 16 + 19 bits
+//
+//  After rounding, keep 24 bits
+//  acc_t should be 24 bits, not 28 bits
 
 module filter_round_truncate(clk, rst_n, acc_in, rf_sat, rf_shift, trig_filter_ovf_flag_clear, filter_out, ro_filter_ovf_flag
     );
@@ -19,21 +29,16 @@ module filter_round_truncate(clk, rst_n, acc_in, rf_sat, rf_shift, trig_filter_o
     input                                   trig_filter_ovf_flag_clear;
     output reg          [15:0]              filter_out;
     output reg                              ro_filter_ovf_flag; 
-
-    reg signed          [46:0]              acc_r;
-    reg signed          [27:0]              acc_t;
+    reg signed          [42:0]              acc_r;
+    reg signed          [23:0]              acc_t;
     wire                [4:0]               num_shift;
-    //reg signed [39:0] round;
     wire                                    sign_bit; 
-    wire                [46:0]              ext_acc_in;         //exact
+    wire                [42:0]              ext_acc_in;
     
     
     assign num_shift = rf_shift + 12; 
     assign sign_bit = acc_in[39];
-    assign ext_acc_in = {{7{sign_bit}}, acc_in};
-
-    // sign bit = val[39] 
-    // ext val = {7[sign bit], val};
+    assign ext_acc_in = {{3{sign_bit}}, acc_in};
 
     always@(posedge clk or negedge rst_n)
     begin 
@@ -45,18 +50,12 @@ module filter_round_truncate(clk, rst_n, acc_in, rf_sat, rf_shift, trig_filter_o
         
         else
         begin
-            //round <= 40'd0;
-            //round[num_shift-1] <= 1'b1;
-            
-            //acc_r <= 47'd0;
-            acc_r <= ext_acc_in + (1<<(num_shift-1));
-            
-            //acc_t <= 28'd0;
-            acc_t <= acc_r[num_shift+:34];//pad
+		  
+            acc_r <= ext_acc_in + (1<<(num_shift-1));            
+            acc_t <= acc_r[num_shift+:23];
             
             if (acc_t > (1<<15)-1)
             begin 
-                $display ("acc_t > (1<<15)-1");
                 ro_filter_ovf_flag <= 1; 
                 if (rf_sat)
                     acc_r <= (1<<15)-1;
@@ -66,14 +65,13 @@ module filter_round_truncate(clk, rst_n, acc_in, rf_sat, rf_shift, trig_filter_o
                 
             else if (acc_t < -(1<<15)) 
             begin
-                $display ("acc_t < -(1<<15) = %d", -(1<<15));
                 ro_filter_ovf_flag <= 1; 
                 if (rf_sat) 
                     acc_r <= (1<<15)-1;
                 else
                     filter_out <= acc_t[15:0];
             end
-                
+				
             else 
                 filter_out <= acc_t[15:0];
 
